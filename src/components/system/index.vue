9e518de6017500ed79b79d7f3aabf0738d0a4ed5
LuChao <atorber@163.com>
2025-04-17 14:05:23 +0800

    更新README.md，添加环境要求和安装步骤；修改侧边栏标题为“设置”；重构系统组件，移除不必要的按钮和逻辑；更新首页和设置页面，增加主题和语言设置功能；调整tauri配置以启用窗口装饰。

--- src/components/system/index.vue
+++ src/components/system/index.vue
@@ -3,78 +3,21 @@
     <el-button v-if="aboutVisible" class="control-el-button" link @click="aboutHandle">
       关于
     </el-button>
-    <el-divider v-if="aboutVisible" direction="vertical" />
-    <el-button
-      v-if="minVisible"
-      class="control-el-button"
-      link
-      @click="minHandle"
-    >
-      <i class="iconfont icon-window-minimize2"></i>
-    </el-button>
-    <el-button
-      v-if="maxVisible"
-      class="control-el-button"
-      link
-      @click="maxHandle"
-    >
-      <i v-if="isMaximized" class="iconfont icon-window-restore"></i>
-      <i v-else class="iconfont icon-window-maximize"></i>
-    </el-button>
-    <el-button class="control-el-button close" link @click="closeHandle">
-      <i class="iconfont icon-window-close"></i>
-    </el-button>
   </div>
 </template>
 <script setup>
   import { getCurrentWebviewWindow } from '@tauri-apps/api/webviewWindow';
-  import { message, confirm } from '@tauri-apps/plugin-dialog';
+  import { message } from '@tauri-apps/plugin-dialog';
   import { getName,getVersion,getTauriVersion } from '@tauri-apps/api/app';
-  import { listen } from '@tauri-apps/api/event'
-  import { onMounted, ref } from 'vue'
+  import { onMounted } from 'vue'
   const appWindow = getCurrentWebviewWindow()
 
   const props = defineProps({
-    minVisible: {
-      type: Boolean,
-      default: () => true,
-    },
-    maxVisible: {
-      type: Boolean,
-      default: () => true,
-    },
     aboutVisible: {
       type: Boolean,
       default: () => false,
     },
   })
-  const isMaximized = ref(false)
-
-  //监视窗体最大化和还原状态，修改对应图标
-  listen('tauri://resize', async() => {
-    isMaximized.value = await appWindow.isMaximized()
-  })
-
-  const minHandle = async () => {
-    await appWindow.minimize()
-  }
-  const maxHandle = async () => {
-    await appWindow.toggleMaximize()
-  }
-  const closeHandle = async () => {
-    if(appWindow.label == 'main'){
-      const confirmation = await confirm(
-        '确定要退出程序吗？',
-        { title: '退出', kind: 'warning' }
-      );
-      if(confirmation)  {
-        appWindow.close()
-      }
-    }
-    else {
-      appWindow.close()
-    }
-  }
 
   const aboutHandle = async () => {
     const aboutMessage =
@@ -85,7 +28,6 @@
   }
 
   onMounted(async () => {
-    isMaximized.value = await appWindow.isMaximized()
   })
 </script>
 <style lang="scss" scoped>
@@ -100,14 +42,11 @@
 
     .control-el-button {
       color: #909399;
+      margin-right: 5px;
       padding: 5px;
       &:hover {
-        background-color: rgba(129, 127, 127, 0.5);
-        color: #ffffff;
-      }
-      &.close:hover {
-        background-color: #f56c6c;
-        color: #ffffff;
+        // background-color: rgba(129, 127, 127, 0.5);
+        color: #000;
       }
     }
   }
